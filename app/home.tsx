cf6693a2e327b14307ae2c90c315a18162ee3ecb
1eehvunzin <hyunjinlee@ewha.ac.kr>
2025-12-01 00:22:23 +0900

    feat: 홈화면 기본 레이아웃 구현

--- app/home.tsx
+++ app/home.tsx
@@ -1,3 +1,28 @@
+import { Image, ImageBackground, View } from "react-native";
+
 export default function Home() {
-  return <h1>home</h1>;
+  return (
+    <View style={{ height: "100vh", width: "100vw" }}>
+      <View style={{ height: "40%", width: "100%" }}>
+        <ImageBackground
+          source={require("../assets/images/BG-sky.png")}
+          style={{ height: "100%", width: "100%", justifyContent: "flex-end" }}
+          resizeMode="cover"
+        >
+          <Image
+            source={require("../assets/images/BG-grass.png")}
+            style={{ width: "100%" }}
+          />
+        </ImageBackground>
+      </View>
+
+      <View style={{ height: "60%", backgroundColor: "brown" }}>
+        <ImageBackground
+          source={require("../assets/images/BG-wood.png")}
+          style={{ height: "100%", width: "100%" }}
+          resizeMode="cover"
+        ></ImageBackground>
+      </View>
+    </View>
+  );
 }
